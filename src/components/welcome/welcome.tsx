a9212df5f591958e840cce5adc0e670d01b489b5
moverq1337 <moverq1337@gmail.com>
2024-10-29 00:02:32 +0300

    maybe got it

--- src/components/welcome/welcome.tsx
+++ src/components/welcome/welcome.tsx
@@ -1,11 +1,14 @@
 import Typewriter from 'typewriter-effect'
 import group1 from '../../assets/group1.svg'
+import group2 from '../../assets/group2.svg'
+import group3 from '../../assets/group3.svg'
+import Button from './buttonio'
 
 export default function Welcome() {
 	return (
-		<div className='text-white'>
-			<img src={group1} alt='' className='blur-[100px] z-0 w-10 h-auto' />
-			<div className='flex ml-[10%] items-center h-screen'>
+		<div className='welcome-container relative text-white h-screen overflow-hidden'>
+			{/* Основной контент */}
+			<div className='flex ml-[10%] items-center h-full relative z-10'>
 				<div className='flex flex-col gap-5'>
 					<h2 className='font-regular text-5xl'>cashready.ru</h2>
 					<h1 className='font-black text-7xl'>GET READY FOR:</h1>
@@ -23,7 +26,19 @@ export default function Welcome() {
 							}}
 						/>
 					</div>
+					<Button></Button>
 				</div>
+			</div>
+
+			{/* Вращающиеся изображения */}
+			<div className='absolute top-[20rem] left-[-20rem]  animate-spin-slow scale-150'>
+				<img src={group1} alt='' className='w-full h-full object-contain' />
+			</div>
+			<div className='absolute top-[-25rem] right-[20rem]  animate-spin-slow scale-150'>
+				<img src={group2} alt='' className='w-full h-full object-contain' />
+			</div>
+			<div className='absolute bottom-[-25rem] left-[90rem] animate-spin-slow scale-150'>
+				<img src={group3} alt='' className='w-full h-full object-contain' />
 			</div>
 		</div>
 	)
