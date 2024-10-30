5bf112716552068dc122b39a0f44e29fdd637eab
unkabas <undercannabis10@gmail.com>
2024-10-30 00:28:31 -0700

    added framer motion

--- src/components/welcome/welcome.tsx
+++ src/components/welcome/welcome.tsx
@@ -1,29 +1,45 @@
+import { motion } from 'framer-motion'
 import Typewriter from 'typewriter-effect'
 import Button from './buttonio'
 
 export default function Welcome() {
 	return (
 		<div className='welcome-container relative text-white h-screen overflow-hidden '>
-			{/* Основной контент */}
-			<div className='flex ml-[10%] items-center h-full relative z-10'>
-				<div className='flex flex-col gap-5'>
-					<h2 className='font-regular text-5xl'>cashready.ru</h2>
-					<h1 className='font-black text-7xl'>GET READY FOR:</h1>
-					<div className='font-semibold text-5xl'>
-						<Typewriter
-							options={{
-								strings: [
-									'bypass any blocking and restrictions',
-									'anonymous on web',
-									'drip collection',
-									'more! more! more!',
-								],
-								autoStart: true,
-								loop: true,
-							}}
-						/>
-					</div>
-					<Button></Button>
+			<div className='flex ml-[10%] items-center h-full relative z-10 '>
+				<div>
+					<motion.div
+						initial={{ y: -300, opacity: 0 }}
+						animate={{ y: 0, opacity: 1 }}
+						transition={{ duration: 1 }}
+					>
+						<div className='flex flex-col gap-10'>
+							<h2 className='font-regular text-5xl'>cashready.ru</h2>
+							<h1 className='font-black text-7xl'>GET READY FOR:</h1>
+							<div className='font-semibold text-5xl'>
+								<Typewriter
+									options={{
+										strings: [
+											'bypass any blocking and restrictions',
+											'anonymous on web',
+											'drip collection',
+											'more! more! more!',
+										],
+										autoStart: true,
+										loop: true,
+									}}
+								/>
+							</div>
+						</div>
+					</motion.div>
+
+					<motion.div
+						initial={{ y: 300, opacity: 0 }}
+						animate={{ y: 0, opacity: 1 }}
+						transition={{ duration: 1 }}
+						className='mt-10'
+					>
+						<Button />
+					</motion.div>
 				</div>
 			</div>
 		</div>
